1118ed21b885ec641cbb14f153dcfb53843670f4
Ashley Anjeles <aanjeles0@gmail.com>
2021-11-17 16:54:30 -0600

    Initial Commit

---
+++ MWT Personal Project/MWT_Personal_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MWT_Personal_ProjectApp.swift
+//  MWT Personal Project
+//
+//  Created by Ashley Anjeles on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MWT_Personal_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
